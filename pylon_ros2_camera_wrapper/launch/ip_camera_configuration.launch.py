2b45ac401454047b8daedab962ae9fe21fb234bf
xuchen <xuchen1522@gmail.com>
2024-04-01 19:14:05 +0900

    asf

--- pylon_ros2_camera_wrapper/launch/ip_camera_configuration.launch.py
+++ pylon_ros2_camera_wrapper/launch/ip_camera_configuration.launch.py
@@ -13,7 +13,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     
-    # adapt if needed
+     # adapt if needed
     debug = False
     respawn = False
 
@@ -23,19 +23,19 @@ def generate_launch_description():
         'ip_camera.yaml'
     )
 
-    # launch configuration variables
+   # launch configuration variables
     node_name = LaunchConfiguration('node_name')
-    camera_id = LaunchConfiguration('camera_id')
-    config_file = LaunchConfiguration('config_file')
+
     # launch arguments
     declare_node_name_cmd = DeclareLaunchArgument(
         'node_name',
-        default_value='ip_auto_config',
-        description='Name of node.'
+        default_value='pylon_ros2_camera_node',
+        description='Name of the wrapper node.'
     )
+
     declare_camera_id_cmd = DeclareLaunchArgument(
         'camera_id',
-        default_value='IP_camera',
+        default_value='ip_camera222',
         description='Id of the camera. Used as node namespace.'
     )
 
@@ -44,35 +44,90 @@ def generate_launch_description():
         default_value=default_config_file,
         description='Camera parameters structured in a .yaml file.'
     )
+
+    declare_mtu_size_cmd = DeclareLaunchArgument(
+        'mtu_size',
+        default_value='1500',
+        description='Maximum transfer unit size. To enable jumbo frames, set it to a high value (8192 recommended)'
+    )
+
+    declare_startup_user_set_cmd = DeclareLaunchArgument(
+        'startup_user_set',
+        # possible value: Default, UserSet1, UserSet2, UserSet3, CurrentSetting
+        default_value='CurrentSetting',
+        description='Specific user set defining user parameters to run the camera.'
+    )
+
+    declare_enable_status_publisher_cmd = DeclareLaunchArgument(
+        'enable_status_publisher',
+        default_value='true',
+        description='Enable/Disable the status publishing.'
+    )
+
+    declare_enable_current_params_publisher_cmd = DeclareLaunchArgument(
+        'enable_current_params_publisher',
+        default_value='true',
+        description='Enable/Disable the current parameter publishing.'
+    )
+
+
+    # launch configuration variables
+    node_name = LaunchConfiguration('node_name')
+    camera_id = LaunchConfiguration('camera_id')
+
+    config_file = LaunchConfiguration('config_file')
+
+    mtu_size = LaunchConfiguration('mtu_size')
+    startup_user_set = LaunchConfiguration('startup_user_set')
+    enable_status_publisher = LaunchConfiguration('enable_status_publisher')
+    enable_current_params_publisher = LaunchConfiguration('enable_current_params_publisher')
+
+
+
+
+
+    # log format
+    os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time} [{name}] [{severity}] {message}'
+
     # see https://navigation.ros.org/tutorials/docs/get_backtrace.html
     if (debug == True):
         launch_prefix = ['xterm -e gdb -ex run --args']
     else:
         launch_prefix = ''
 
-
     # node
-    ip_auto_config_node = Node(
-        package='pylon_ros2_camera_component',
+    pylon_ros2_camera_node = Node(
+        package='pylon_ros2_camera_wrapper',        
         namespace=camera_id,
-        executable='ip_auto_config',
+        executable='pylon_ros2_camera_wrapper',
         name=node_name,
         output='screen',
         respawn=respawn,
         emulate_tty=True,
         prefix=launch_prefix,
-        prefix=['xterm -e gdb -ex run --args'],
-        parameters=[config_file]
+        parameters=[
+            config_file,
+            {
+                'gige/mtu_size': mtu_size,
+                'startup_user_set': startup_user_set,
+                'enable_status_publisher': enable_status_publisher,
+                'enable_current_params_publisher': enable_current_params_publisher
+            }
+        ]
     )
 
     # Define LaunchDescription variable and return it
     ld = LaunchDescription()
-    
+
     ld.add_action(declare_node_name_cmd)
     ld.add_action(declare_camera_id_cmd)
     ld.add_action(declare_config_file_cmd)
+    
+    ld.add_action(declare_mtu_size_cmd)
+    ld.add_action(declare_startup_user_set_cmd)
+    ld.add_action(declare_enable_status_publisher_cmd)
+    ld.add_action(declare_enable_current_params_publisher_cmd)
 
-
-    ld.add_action(ip_auto_config_node)
+    ld.add_action(pylon_ros2_camera_node)
 
     return ld
